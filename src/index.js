f971627869ebe6b2d6b64564a0a2fe1d34b6674d
Adam Butterworth <abutterworth@edx.org>
2019-01-04 11:22:29 -0500

    move secondary menu items into constants file

--- src/index.js
+++ src/index.js
@@ -6,76 +6,7 @@ import './index.scss';
 
 import EdxLogo from './edx-sm.png';
 
-import { MENU_ITEMS } from './MenuItems.constants';
-
-const SECONDARY_MENU_ITEMS_DESKTOP = [
-  // {
-  //   content: "Sign in",
-  //   triggerClassName: "btn-link",
-  //   name: "sign-in",
-  //   destination: "#",
-  // },
-  // {
-  //   content: "Register",
-  //   triggerClassName: "btn-primary",
-  //   name: "register",
-  //   destination: "#",
-  // },
-  {
-    content: (<span>My Account</span>),
-    triggerClassName: "btn-outline-secondary",
-    name: "account",
-    submenu: (
-      <div>
-      <a className="btn btn-primary btn-block mt-2 mb-3" href="#">Resume my last course</a>
-      <ul className="nav flex-column">
-        <li className="nav-item"><a className="nav-link" href="#">My Dashboard</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">My Courses</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">My Programs</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Help</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">My Profile</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Account Settings</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Sign Out</a></li>
-      </ul>
-      </div>
-    )
-  },
-]
-
-
-const SECONDARY_MENU_ITEMS_MOBILE = [
-  {
-    content: "Sign in",
-    triggerClassName: "btn-link",
-    name: "sign-in",
-    destination: "#",
-  },
-  {
-    content: "Register",
-    triggerClassName: "btn-primary",
-    name: "register",
-    destination: "#",
-  },
-  {
-    content: (<span>My Account</span>),
-    triggerClassName: "btn-outline-secondary",
-    name: "account",
-    submenu: (
-      <div>
-      <a className="btn btn-primary btn-block mt-2 mb-3" href="#">Resume my last course</a>
-      <ul className="nav flex-column">
-        <li className="nav-item"><a className="nav-link" href="#">My Dashboard</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">My Courses</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">My Programs</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Help</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">My Profile</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Account Settings</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Sign Out</a></li>
-      </ul>
-      </div>
-    )
-  },
-]
+import { MENU_ITEMS, SECONDARY_MENU_ITEMS } from './MenuItems.constants';
 
 const App = () => (
   <div>
@@ -85,7 +16,7 @@ const App = () => (
       logo={EdxLogo}
       logoDestination="https://edx.org"
       logoAltText="edX"
-      secondaryMenuItems={SECONDARY_MENU_ITEMS_DESKTOP}
+      secondaryMenuItems={SECONDARY_MENU_ITEMS}
     /> 
   </div>
 );
